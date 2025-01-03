4ba844e9c2c5021809f7cc457147966767333c7a
labb <labb@rssoftware.co.in>
2025-01-03 19:40:38 +0530

    added

---
+++ online/API_gateway/src/main/java/com/rssoftware/intelliedge/online/apigateway/Application.java
@@ -0,0 +1,38 @@
+package com.rssoftware.intelliedge.online.apigateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.rssoftware.intelliedge.common.expr.parser",
+		"com.rssoftware.intelliedge.online.apigateway.endpoint",
+		"com.rssoftware.intelliedge.online.apigateway.transformation.impl",
+		"com.rssoftware.intelliedge.online.apigateway.service.impl",
+		"com.rssoftware.intelliedge.online.apigateway.response.creator"})
+//@Import(ServiceContext.class)
+@Slf4j
+public class Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext configurableApplicationContext =
+				SpringApplication.run(Application.class, args);
+
+		log.info("Application Started:{}", configurableApplicationContext);
+	}
+
+
+	/**
+	 * @param args agrument
+	 * @throws Exception
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
